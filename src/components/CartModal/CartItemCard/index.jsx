12a3209527edf8ed613e53adb6a4ea086b1c1748
Alice Ribeiro Paz da Rosa <alicerosamat@gmail.com>
2024-03-22 15:31:07 -0300

    Correção

--- src/components/CartModal/CartItemCard/index.jsx
+++ src/components/CartModal/CartItemCard/index.jsx
@@ -1,15 +1,47 @@
 import { MdDelete } from "react-icons/md";
+import styles from "../../../styles/style.module.scss"
+import { useEffect, useState } from "react";
 
-export const CartItemCard = ({ product }) => {
+
+export const CartItemCard = ({ children, product }) => {
+   const [newLocalChart, setNewLocalChart] = useState([])
+   const chartData = localStorage.getItem("@CHART");
+   useEffect(() => {
+      
+      if (chartData) {
+        setNewLocalChart(JSON.parse(chartData));
+      }
+    }, []);
+    
+    const removeItem = (productId) => {
+      const newChart = newLocalChart.filter(element => element.id !== productId);
+      setNewLocalChart(newChart);
+    }
+    
+  
+  
+
+   useEffect (()=>{
+      localStorage.setItem("@CHART", JSON.stringify(newLocalChart))
+
+
+   },[newLocalChart])
+
    return (
-      <li>
-         <div>
-            <img src={product.img} alt={product.name} />
-            <h3>{product.name}</h3>
-         </div>
-         <button aria-label="delete" title="Remover item">
+      <li className={styles.liModal}>
+         <div className={styles.itemCard}>
+            <img src={product.img} alt={product.name} className={styles.imgModal}/>
+            <div className={styles.center}>
+               <h3>{product.name}</h3>
+               <p>{product.price.toLocaleString('pt-BR', { style: "currency", currency: "BRL"})}</p>
+            </div>
+            <button aria-label="delete" title="Remover item" className={styles.removeButton} onClick={()=>removeItem(product.id)}>
             <MdDelete size={21} />
          </button>
+         {children}
+            
+         </div>
+         
       </li>
    );
 };
--- src/components/ProductList/ProductCard/index.jsx
+++ src/components/ProductList/ProductCard/index.jsx
@@ -1,13 +1,27 @@
+import { useEffect } from "react";
+import style from "../../../styles/style.module.scss";
+
 export const ProductCard = ({ product }) => {
-    return(
-        <li>
+    const addToCart = (newProduct) => {
+        const localChart = JSON.parse(localStorage.getItem("@CHART")) || [];
+        
+        if (!localChart.some(cartItem => cartItem.id === newProduct.id)) {
+            const updatedCart = [...localChart, newProduct];
+            localStorage.setItem("@CHART", JSON.stringify(updatedCart));
+        } else {
+            alert("Produto já adicionado ao carrinho.");
+        }
+    };
+
+    return (
+        <li className={style.liPrincipal}>
             <img src={product.img} alt={product.name} />
             <div>
                 <h3>{product.name}</h3>
                 <span>{product.category}</span>
                 <span>{product.price.toLocaleString('pt-BR', { style: "currency", currency: "BRL"})}</span>
-                <button>Adicionar</button>
+                <button onClick={() => addToCart(product)}>Adicionar</button>
             </div>
         </li>
-    )
-}
+    );
+};
