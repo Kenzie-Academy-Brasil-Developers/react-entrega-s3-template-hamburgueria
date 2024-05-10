d9c0eb0a5fa11ba74bf1b90bec7d7b715788a042
Carlos Lucas Sales de Andrade <132520068+lucassale@users.noreply.github.com>
2024-05-09 21:16:12 -0300

    Estilizando HomePage (main)

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,26 +1,28 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Logo from "../../assets/Logo.svg";
 import { MdSearch, MdShoppingCart } from "react-icons/md";
+import "./Header.scss"; // Importando o arquivo SCSS
 
 export const Header = () => {
    const [value, setValue] = useState("");
 
    return (
-      <header>
-         <img src={Logo} alt="Logo Kenzie Burguer" />
-         <div>
-            <button>
-                <MdShoppingCart size={21} />
-                <span>0</span>
+      <header className="header">
+         <img src={Logo} alt="Logo Kenzie Burguer" className="logo" />
+         <div className="header-content">
+            <button className="header-button">
+                <MdShoppingCart size={21} className="header-icon" />
+                <span className="header-cart-counter">0</span>
             </button>
-            <form>
+            <form className="header-form">
                <input
                   type="text"
                   value={value}
                   onChange={(e) => setValue(e.target.value)}
+                  className="header-input"
                />
-               <button type="submit">
-                 <MdSearch size={21} />
+               <button type="submit" className="header-submit-button">
+                 <MdSearch size={21} className="header-icon" />
                </button>
             </form>
          </div>
